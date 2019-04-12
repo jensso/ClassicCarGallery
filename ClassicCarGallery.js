fd747a0e398131f7366acdf9ff999b23d5722d17
jens soltwedel <jens.soltwedel@hotmail.com>
2019-04-12 09:02:21 +0200

    evLis for close Big img added

--- ClassicCarGallery.js
+++ ClassicCarGallery.js
@@ -1,8 +1,8 @@
-const fetchBtn = document.querySelector(`button`);
+const fetchBtn = document.querySelector('button');
 
 const getImage = async (ev) => {
-  ev.currentTarget.removeEventListener(`click`,getImage);
-  const imageFolder = `./src/carInfo.json`;
+  ev.currentTarget.removeEventListener('click',getImage);
+  const imageFolder = './src/carInfo.json';
   const imageLink = await fetch(imageFolder);
   const imageAddress = await imageLink.json();
 try {
@@ -17,12 +17,12 @@ for (let i = 0; i < imageAddress.length; i++) {
         let carToShow = await delayFetch(imageAddress[i],1500);
         await carToShow.json();
 
-      let container = document.querySelector(`#container`);
-      let section = document.createElement(`SECTION`);
-      let image = document.createElement(`IMG`);
+      let container = document.querySelector('#container');
+      let section = document.createElement('SECTION');
+      let image = document.createElement('IMG');
       let span = document.createElement('SPAN');
-      let header = document.createElement(`H2`);
-      let paragraph = document.createElement(`P`);
+      let header = document.createElement('H2');
+      let paragraph = document.createElement('P');
 
       image.src = imageAddress[i].address;
       span.innerText = 'click the car to enlarge view';
@@ -36,22 +36,26 @@ for (let i = 0; i < imageAddress.length; i++) {
       section.appendChild(paragraph);
       document.body.appendChild(container);
 
-  const biggerPicture = image.addEventListener(`click`, (ev)=> {
-        let newSection = document.createElement(`SECTION`);
-        newSection.setAttribute(`id`, `newSection`);
-        let newPicture = document.createElement(`IMG`);
-        newPicture.setAttribute(`id`,`newPicture`);
-        let closeSpan = document.createElement(`SPAN`);
-        closeSpan.setAttribute(`id`,`closeSpan`);
-        closeSpan.innerText = `X`;
+  const biggerPicture = image.addEventListener('click', (ev)=> {
+        let newSection = document.createElement('SECTION');
+        newSection.setAttribute('id', 'newSection');
+        let newPicture = document.createElement('IMG');
+        newPicture.setAttribute('id','newPicture');
+        let closeSpan = document.createElement('SPAN');
+        closeSpan.setAttribute('id','closeSpan');
+        closeSpan.innerText = 'X';
         newPicture.src = imageAddress[i].address;
         newSection.appendChild(newPicture);
         newSection.appendChild(closeSpan);
         document.body.appendChild(newSection);
 
-        closeSpan.addEventListener(`click`, (ev)=> {
+        closeSpan.addEventListener('click', (ev)=> {
         document.body.removeChild(newSection);
           })
+        newSection.addEventListener('click', (ev)=> {
+          document.body.removeChild(newSection);
+            })
+
       })
     }
   }
@@ -60,4 +64,4 @@ catch (error) {
     }
 };
 
-fetchBtn.addEventListener(`click`,getImage);
+fetchBtn.addEventListener('click',getImage);
